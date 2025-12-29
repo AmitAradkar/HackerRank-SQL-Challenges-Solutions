1c9cace38a76890f69236b170448e867f251e894
AmitAradkar <amitaradkar@gmail.com>
2025-12-29 10:06:40 +0530

    Update Aggreate_Weather Observation Station 20.sql

--- SQL-Basic-Select/Aggreate_Weather Observation Station 20.sql
+++ SQL-Basic-Select/Aggreate_Weather Observation Station 20.sql
@@ -17,5 +17,25 @@ the STATION table and round the result to 4 decimal places.
 */
 
 SELECT 
-    ROUND(MEDIAN(LAT_N), 4) AS median_lat
-FROM STATION;
+    ROUND(AVG(LAT_N), 4) AS median
+FROM (
+    SELECT 
+        LAT_N,
+        ROW_NUMBER() OVER (ORDER BY LAT_N) AS rn,
+        COUNT(*) OVER () AS cnt
+    FROM STATION
+) t
+WHERE rn IN (FLOOR((cnt + 1) / 2), CEIL((cnt + 1) / 2));
+
+or 
+
+SELECT 
+    ROUND(AVG(LAT_N), 4) AS median
+FROM (
+    SELECT LAT_N
+    FROM STATION
+    ORDER BY LAT_N
+    LIMIT 2 - (SELECT COUNT(*) FROM STATION) % 2
+    OFFSET (SELECT (COUNT(*) - 1) / 2 FROM STATION)
+) t;
+
